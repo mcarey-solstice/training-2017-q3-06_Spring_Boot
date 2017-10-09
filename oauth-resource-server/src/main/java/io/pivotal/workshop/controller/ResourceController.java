b3e3fbed0ddc922cda73af48f29fb5f3ffd427bd
Mike Carey <mcarey@solstice.com>
2017-10-09 18:09:19 -0500

    OAuth - Resource Server

---
+++ oauth-resource-server/src/main/java/io/pivotal/workshop/controller/ResourceController.java
@@ -0,0 +1,14 @@
+package io.pivotal.workshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/")
+    public String helloSecured() {
+        return "Hello - Secured";
+    }
+
+}
